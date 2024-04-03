00a0ae3ace0d24bd10538dbc8146e46ea5955c18
Elm2006 <isel06002@utb.vaxjo.se>
2024-04-03 11:03:22 +0200

    Add project files.

---
+++ OOP övning 2/Namn.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Namn
+{
+    private string förnamn;
+    private string efternamn;
+
+    public string Förnamn
+    {
+        get { return förnamn; }
+
+        set { förnamn = value; }
+    }
+    public string Efternamn
+    {
+        get { return efternamn; }
+
+        set { efternamn = value; }
+    }
+    public void FullNamn()
+    {
+        string helnamn = förnamn + " " + efternamn;
+        Console.WriteLine(helnamn);
+    }
+}
